dee423a64b7ae0b73c3f31402e274acec05a997a
lennonjo <lennonjo@sulaco>
2016-02-05 20:52:08 +0000

    Pushing up new project created in eclipse

---
+++ bitslicer/bitslice.h
@@ -0,0 +1,17 @@
+/*
+ * bitslice.h
+ *
+ *  Created on: 5 Feb 2016
+ *      Author: lennonjo
+ */
+
+#ifndef BITSLICE_H_
+#define BITSLICE_H_
+
+class bitslice {
+public:
+	bitslice();
+	virtual ~bitslice();
+};
+
+#endif /* BITSLICE_H_ */
